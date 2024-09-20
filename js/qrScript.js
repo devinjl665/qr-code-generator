858594e4357eb38671557882a0ff0c2c4b9aa7f4
elroy <devinjl665@gmail.com>
2024-09-20 10:54:37 -0500

    Used diff library now able to load qr code

--- js/qrScript.js
+++ js/qrScript.js
@@ -7,35 +7,37 @@ const onGenerateSubmit = (e) => {
     const url = document.getElementById('url').value;
     const size = document.getElementById('size').value;
 
+    console.log(url, size);
+
     if (url === '') {
-        alert('Please enter a URL');
-        return;
+        alert ('Please enter URL');
     } else {
         showSpinner();
 
-    setTimeout(() => {
-        hideSpinner();
-        generateQRCode(url, size);
-    }, 1000);
+        setTimeout(() => {
+            hideSpinner();
+            generateQRCode(url,size);
+        }, 1000);
     }
 };
 
 const generateQRCode = (url, size) => {
-    const qrcode = new QRCode('qrcode', {
+    const qrcode = new QRCode("qrcode", {
         text: url,
         width: size,
-        height: size,
+        height: size
     });
 };
 
 const showSpinner = () => {
     document.getElementById('spinner').style.display = 'block';
-};
 
+}
 
 const hideSpinner = () => {
     document.getElementById('spinner').style.display = 'none';
-};
+
+}
 
 hideSpinner();
 
